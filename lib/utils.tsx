7f9f6275381ed512cb46c7cdb87fff7f82a43aeb
Łukasz <lukasz.poczta12@gmail.com>
2024-02-15 10:03:32 +0100

    modify main page

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -1,9 +1,10 @@
+import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from 'clsx';
+
 export const handleCall = () => {
   const phoneNumber = '+48608450246';
   window.location.assign(`tel:${phoneNumber}`);
 };
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
